7ed5c55cbaf251ff5963f70b3f1464a92af13d04
Oleksii Yakuba <alexeyyakuba@gmail.com>
2021-07-10 22:33:42 +0300

    10.07

--- SPLIT/js/app.js
+++ SPLIT/js/app.js
@@ -790,6 +790,44 @@ animate({
 			Element.prototype.msMatchesSelector;
 	}
 })();
+let main_slider = new Swiper(".main-slider", {
+  // effect: "fade",
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: true,
+  // },
+  observer: true,
+  observeParents: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // autoHeight: true,
+  speed: 800,
+  //touchRatio: 0,
+  //simulateTouch: false,
+  //loop: true,
+  preloadImages: false,
+  lazy: true,
+  // Dotts
+  pagination: {
+    el: ".swiper-pagination",
+    type: 'bullets',
+    clickable: true,
+  },
+  // Arrows
+  // navigation: {
+  //   nextEl: ".about__more .more__item_next",
+  //   prevEl: ".about__more .more__item_prev",
+  // },
+  on: {
+    lazyImageReady: function () {
+      ibg();
+    },
+  },
+  // And if we need scrollbar
+  //scrollbar: {
+  //	el: '.swiper-scrollbar',
+  //},
+});
 
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
